1d0dea1f5caefb3111a41cfc17ccfa2b5c6127b3
Gabriel <gabriel.gressie@gmail.com>
2020-04-22 22:46:16 +0200

    added a nested component

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+Vue.component('task-list', {
+    template: '<div><task v-for="task in tasks">{{ task.task }}</task></div>',
+
+    data() {
+        return {
+            tasks: [
+                { task: 'Go to the Store', completed: true },
+                { task: 'Go to the email', completed: false },
+                { task: 'Go to the farm', completed: true },
+                { task: 'Go to the work', completed: false },
+            ]
+        };
+    }
+});
+
+
 Vue.component('task', {
     template: '<li><slot></slot></li>'
 
