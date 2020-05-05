a39a246dd25a1117ed374ebe43a32749cd5e94b5
Gabriel <gabriel.gressie@gmail.com>
2020-05-05 22:02:24 +0200

    added some tests with hmr

---
+++ lesson/src/components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>The Count Be: {{ count }}</h2>
+
+    <p>
+      <button @click="count += 1">Add one to counter</button>
+    </p>
+  </div>
+</template>
+
+<script>
+    export default {
+        data() {
+          return {
+            count: 0
+          };
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ myVueApp/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <message>Hi World</message>
+    <message>Hi World Again</message>
+    <message>Ik snap het niet</message>
+  </div>
+</template>
+
+<script>
+  import Message from './components/Message.vue';
+
+  export default {
+    name: 'app',
+    components: { Message },
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+ <style>
+
+ </style>
