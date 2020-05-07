e03f73b0f53b88c4af67a6a087c4575b8f7afbad
Gabriel <gabriel.gressie@gmail.com>
2020-05-07 16:08:59 +0200

    added a tooltip example

---
+++ vue-app/resources/js/components/Tooltip.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div ref="body" v-show="false">
+            <slot></slot>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import PopperTooltip from 'tooltip.js';
+
+    export default {
+        name: "Tooltip",
+        props: {
+            name: {}
+        },
+
+        mounted() {
+            document
+                .querySelectorAll(`[data-tooltip-name=${this.name}]`)
+                .forEach(elem => {
+                    new PopperTooltip(elem, {
+                        placement: 'left',
+                        title: this.$refs.body.innerHTML,
+                        html: true
+                    });
+                });
+
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
--- vue-app/resources/views/welcome.blade.php
+++ vue-app/resources/views/welcome.blade.php
@@ -3,17 +3,49 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.4.5/tailwind.min.css">
         <title>Laravel</title>
+        <style>
+            html, body { height: 100vh; }
 
+            body {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
 
+            .tooltip {
+                background: black;
+                color: white;
+                padding: 10px;
+            }
+        </style>
     </head>
     <body>
         <div id="app">
+            <h3>Option #1</h3>
+            <p>
+                Hallo allemaal, <span data-tooltip="Boo!">hover eens over mij?</span>
+            </p>
+
+            <h3>Option #2</h3>
+            <p>
+                Bonjour, <span v-tooltip:right="'A 2nd way to display a tooltip'">hover pour moi</span>
+            </p>
+
+            <h3>Option #3</h3>
+            <p>
+                Hello Everyone, <span data-tooltip-name="third-sample-tooltip">give me a hover, wouldya?</span>
+            </p>
+
+            <tooltip name="third-sample-tooltip">
+                <h1>Thank you!</h1>
+                <p>
+                    You're getting good at this hovering thing, my friend :)
+                </p>
+            </tooltip>
 
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.js"></script>
-        <script src="/app.js"></script>
+        <script src="/js/app.js"></script>
     </body>
 </html>
